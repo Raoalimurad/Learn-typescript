14fb7b20934da37e3bcb32228d9b004b5d123aa3
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-30 11:33:37 +0500

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,13 @@
+// /////there are many data types
+// string
+// boolean
+// null
+// number
+var fname = "Rao Ali murad";
+console.log(typeof fname);
+var num1 = 50;
+console.log(typeof num1);
+var boll = true;
+console.log(typeof boll);
+var nul = null;
+console.log(typeof nul);
---
+++ datatype.ts
@@ -0,0 +1,20 @@
+// /////there are many data types
+// string
+// boolean
+// null
+// number
+
+
+
+
+let fname="Rao Ali murad";
+console.log(typeof fname)
+
+let num1=50;
+console.log(typeof num1);
+
+let boll=true;
+console.log(typeof boll);
+
+let nul=null;
+console.log(typeof nul);
