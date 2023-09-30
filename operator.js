e7d8374b4e9a5b8a01cf873e22dffb71dcb1c118
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-30 12:06:33 +0500

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,13 @@
+// operators
+// addition
+var num1 = 30;
+var num2 = 20;
+console.log(num1 + num2);
+// substraction
+console.log(num1 - num2);
+////multiplication
+console.log(num1 * num2);
+// division
+console.log(num1 / num2);
+// modulus.
+console.log(num1 % num2);
---
+++ operator.ts
@@ -0,0 +1,26 @@
+// operators
+
+// addition
+let num1=30;
+let num2= 20;
+console.log(num1+num2)
+
+// substraction
+console.log(num1-num2)
+
+
+////multiplication
+
+console.log(num1 * num2)
+
+
+
+// division
+
+console.log(num1/num2)
+
+
+
+
+// modulus.
+console.log(num1%num2)
