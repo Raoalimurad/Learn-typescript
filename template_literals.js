e4cb378287e0e220db5c5bbfa9b646d7b4d0619e
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-30 11:44:40 +0500

    Add files via upload

---
+++ template_literals.js
@@ -0,0 +1,7 @@
+// a new and fast way to deal with string is called template literals
+// before templates literals we deal like this
+var fname = "Rao ali murad";
+console.log("my name is", fname);
+// now use template literals
+var uni = "sindh madarsatul islam";
+console.log("my uni name is ".concat(uni)); //////this is template literals
---
+++ template_literals.ts
@@ -0,0 +1,10 @@
+// a new and fast way to deal with string is called template literals
+
+// before templates literals we deal like this
+let fname="Rao ali murad";
+console.log("my name is",fname);
+
+
+// now use template literals
+let uni="sindh madarsatul islam";
+console.log(`my uni name is ${uni}`)//////this is template literals
