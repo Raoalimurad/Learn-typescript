3ff5b460a91ce9a3fbd99085ade306ee08b2048d
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-30 15:10:19 +0500

    Add files via upload

---
+++ if-else_statment.js
@@ -0,0 +1,38 @@
+//its use for condition 
+var age = 17;
+if (age >= 18) {
+    console.log("you can apply");
+}
+// if condition wrong then we print else statment
+if (age >= 18) {
+    console.log("you can apply");
+}
+else {
+    console.log("you can not apply");
+}
+// Now use else if condition
+var day = "tuesday";
+if (day == "sunday") {
+    console.log("today is sunday");
+}
+else if (day == "monday") {
+    console.log("today is monday");
+}
+else if (day == "tuesday") {
+    console.log("today is tuesday");
+}
+else if (day == "wednesday") {
+    console.log("today is wednesday");
+}
+else if (day == "thursday") {
+    console.log("today is thursday");
+}
+else if (day == "friday") {
+    console.log("today is friday");
+}
+else if (day == "saturday") {
+    console.log("today is saturday");
+}
+else {
+    console.log("invalid");
+}
