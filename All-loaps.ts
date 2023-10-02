fdc3b23d4da1e13d61cf6ab8ad1fd8e5bf869855
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-02 11:59:18 +0500

    Add files via upload

---
+++ All-loaps.ts
@@ -0,0 +1,20 @@
+////for loap we use for reptation for example we want to print 100 time anything you can print using loap;
+for(let i=0; i<=20;i++){
+    console.log("Hello Rao Ali Murad");
+}
+
+/////WHILE LOAP
+let num:number=0;
+while(num<=10){
+    console.log("hello while loap")
+    num++;
+}
+
+////do while loap
+
+/////DO WHILE HAS A DIFFERNET PROPERTY INDEED YOUR CONDITON IS WRONG ITS STATMENT WILL EXCUTE ONE TIME;
+
+let num1=10;
+do{
+    console.log("hello do while loap")
+}while(num1>20)////conditon is wrong but it will excute one time
