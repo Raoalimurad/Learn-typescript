c5f225f8c67acc3468dada4a10bb90cd27f590bd
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-02 09:23:17 +0500

    Add files via upload

---
+++ Logical_operators.js
@@ -0,0 +1,22 @@
+var country = "pakistan";
+var age = 18;
+if (country == "pakistan" && age == 18) {
+    console.log("you are eligible");
+}
+else {
+    console.log("you are not eligible");
+}
+//////OR operator  means true false= true
+if (country == "pakistan" || age == 19) {
+    console.log("you are eligible");
+}
+else {
+    console.log("you are not eligible");
+}
+/////not operator
+if (!(age = 18)) {
+    console.log("you are in 18");
+}
+else {
+    console.log("you are not");
+}
---
+++ Logical_operators.ts
@@ -0,0 +1,26 @@
+let country:string="pakistan";
+let age:number=18
+if(country =="pakistan" && age==18){
+    console.log("you are eligible");
+}else{
+    console.log("you are not eligible");
+}
+
+
+//////OR operator  means true false= true
+if(country =="pakistan"||age==19){
+    console.log("you are eligible");
+}else{
+    console.log("you are not eligible");
+}
+
+
+/////not operator
+
+
+if(!(age=18)){
+    console.log("you are in 18")
+}else{
+    console.log("you are not")
+}
+
