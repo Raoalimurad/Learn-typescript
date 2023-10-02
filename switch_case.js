9ee21a24ca71171ec8deb9d7b8a3e5bf3d5c3de3
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-02 12:11:49 +0500

    Add files via upload

---
+++ switch_case.js
@@ -0,0 +1,26 @@
+var days = 1;
+for (var i = 0; i <= 7; i++) {
+    switch (i) {
+        case 1:
+            console.log("today is monday");
+            break;
+        case 2:
+            console.log("today is tuesday");
+            break;
+        case 3:
+            console.log("today is wednesday");
+            break;
+        case 4:
+            console.log("today is thursday");
+            break;
+        case 5:
+            console.log("today is friday");
+            break;
+        case 6:
+            console.log("today is saturday");
+            break;
+        case 7:
+            console.log("today is sunday");
+            break;
+    }
+}
