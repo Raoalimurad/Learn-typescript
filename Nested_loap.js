9f341845819a785e5ba6c0a29e5ede14578d79aa
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-02 13:04:57 +0500

    Add files via upload

---
+++ Nested_loap.js
@@ -0,0 +1,6 @@
+for (var a = 0; a <= 30; a = a + 10) {
+    for (var b = 0; b <= 10; b++) {
+        console.log(b);
+    }
+    console.log(a);
+}
---
+++ Nested_loap.ts
@@ -0,0 +1,7 @@
+for(let a=0; a<=30; a=a+10){
+   
+     for(let b=0; b<=10;b++){
+        console.log(b)
+     }
+     console.log(a)
+}
