7f446147ab0025199b67b50e100960cf6eb465e2
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-05 14:03:36 +0500

    Add files via upload

---
+++ ternary_operator.js
@@ -0,0 +1,4 @@
+var username = "rao";
+(username == "Rao Ali Murad" || username == "rao") ? console.log("you logged in ") : console.log("you are not");
+// Syntax
+// condition ?  code1  true :code2 false
---
+++ ternary_operator.ts
@@ -0,0 +1,8 @@
+let username:string="rao";
+
+(username=="Rao Ali Murad" || username=="rao")? console.log("you logged in "):console.log("you are not")
+
+
+
+// Syntax
+// condition ?  code1  true :code2 false
