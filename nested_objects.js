92c7319a24775b707ce861d01a1b8a5f211c7c22
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-06 14:16:34 +0500

    Add files via upload

---
+++ nested_objects.js
@@ -0,0 +1,10 @@
+var obj1 = {
+    name: "Rao Ali Murad",
+    age: 19,
+    obj2: {
+        email: "raoalimurad@",
+        id: 15,
+    }
+};
+console.log(obj1.age);
+console.log(obj1.obj2.email); ////acess property of nested object
---
+++ nested_objects.ts
@@ -0,0 +1,11 @@
+let obj1={
+    name:"Rao Ali Murad",
+    age:19,
+    obj2:{
+        email:"raoalimurad@",
+        id:15,
+    }
+
+}
+console.log(obj1.age)
+console.log(obj1.obj2.email)////acess property of nested object
