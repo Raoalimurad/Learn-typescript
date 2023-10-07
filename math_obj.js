e987431d93804cad1d4b0fbd2ddb38342fcc8491
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-07 17:00:08 +0500

    Add files via upload

---
+++ math_obj.js
@@ -0,0 +1,28 @@
+////round method 
+var x = 10.4;
+var round = Math.round(x);
+console.log(round);
+/////ceil method ya value ko point ka bad value pe la jatta hy means 10.1 hy to 11 ho jae ga
+var y = 10.2;
+var ceilMethod = Math.ceil(y);
+console.log(ceilMethod);
+/////floor mrthod its minimum roundof
+var z = 9.5;
+var floorMethod = Math.floor(z);
+console.log(floorMethod);
+////trunc method drop all value after point
+var a = 12.5;
+var trunk = Math.trunc(a);
+console.log(trunk);
+// power method
+var power = Math.pow(5, 2); ///means 5^2 2 is power on 5
+console.log(power);
+// square root
+var square = Math.sqrt(25);
+console.log(square);
+////minimum method
+var minimum = Math.min(100, 200, 500, 1000);
+console.log(minimum);
+/////maximum method
+var maximum = Math.max(100, 1000, 10, 90);
+console.log(maximum);
---
+++ math_obj.ts
@@ -0,0 +1,36 @@
+////round method 
+let x:number=10.4
+let round= Math.round(x)
+console.log(round)
+
+/////ceil method ya value ko point ka bad value pe la jatta hy means 10.1 hy to 11 ho jae ga
+let y:number=10.2;
+let ceilMethod=Math.ceil(y);
+console.log(ceilMethod);
+
+/////floor mrthod its minimum roundof
+let z:number=9.5;
+let floorMethod=Math.floor(z);
+console.log(floorMethod);
+
+////trunc method drop all value after point
+let a=12.5;
+let trunk=Math.trunc(a);
+console.log(trunk)
+
+// power method
+let power=Math.pow(5,2)///means 5^2 2 is power on 5
+console.log(power)
+
+// square root
+
+let square=Math.sqrt(25);
+console.log(square);
+
+////minimum method
+let minimum=Math.min(100,200,500,1000)
+console.log(minimum);
+
+/////maximum method
+let maximum=Math.max(100,1000,10,90)
+console.log(maximum)
