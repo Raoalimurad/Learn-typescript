74fbf401af25f58aab5d62e3019e98e12f0a4d1a
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-12 10:16:47 +0500

    Add files via upload

---
+++ lexical_envirment.js
@@ -0,0 +1,10 @@
+function outer() {
+    var outerVar = 'I am from outer scope';
+    function inner() {
+        var innerVar = 'I am from inner scope';
+        console.log(outerVar); // inner() can access outerVar
+    }
+    inner();
+    // console.log(innerVar); // This will result in an error since innerVar is not defined in the outer scope.
+}
+outer();
